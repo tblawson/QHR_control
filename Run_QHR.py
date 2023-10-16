6e79a705cc31d07f3c178b906f3295cc75015638
Tim Lawson <tim.lawson@callaghaninnovation.govt.nz>
2023-10-16 16:58:27 +1300

    Added universal delay of 0.1 s to instrument_control.py. ALL instrument readings lists are now attributes of the SMS instance. Added a switch to print the message in read_buffer(). Added doc strings to most methods. Improved _extract_fieldvalue() method. Added data storage section (needs finishing).

--- Run_QHR.py
+++ Run_QHR.py
@@ -5,6 +5,7 @@ Plot V vs B and simultaneously write data to csv files.
 """
 
 
+import os
 import pyvisa as visa
 import csv
 import instrument_control as control
@@ -14,31 +15,32 @@ print(rm.list_resources())
 
 # Set up magnet as a visa resource:
 magnet_addr_str = control.gpibaddr_num2str(input('magnet GPIB addr? > '))
+# magnet_addr_str = 'GPIB0::4::INSTR'  # control.gpibaddr_num2str('4')
 magnet_interface = rm.open_resource(magnet_addr_str)
 magnet_interface.clear()
 magnet = control.SMS(magnet_interface)  # Create SMS object called 'magnet'
 magnet.show_sign_on_msg()  # Print multi-line message, summarising magnet supply state.
 
 # Set up dvm as a visa resource
-dvm_addr_str = control.gpibaddr_num2str(input('dvm GPIB addr? > '))
+# dvm_addr_str = control.gpibaddr_num2str(input('dvm GPIB addr? > '))
+dvm_addr_str = 'GPIB0::25::INSTR'  # control.gpibaddr_num2str('25')
 dvm = rm.open_resource(dvm_addr_str)
+dvm.timeout = 2000
+dvm.read_termination = '\r\n'
+dvm.write_termination = '\r\n'
 
-"""
-Magnet control-loop 1.
-"""
 QUIT = ['q', 'Q', 'quit', 'Quit', 'QUIT', 'exit', 'Exit', 'EXIT']
-CMD_ALIASES = {'mid': 'ramp mid',
-               'max': 'ramp max',
-               'zero': 'ramp zero',
-               'stat': 'ramp status',
-               'out': 'get output'}
+RAMP_ALIASES = {'mid': 'ramp mid',
+                'max': 'ramp max',
+                'zero': 'ramp zero'}
+CMD_ALIASES = RAMP_ALIASES | {'stat': 'ramp status', 'out': 'get output'}  # Add 2 extra items
+
 prev_cmd = '*IDN?'  # default command
 while True:
     """
-    Use this control loop as a sandbox to explore magnet functions.
-    and set up ramp limits - But NOT for acquiring data.
+    Main control loop
     """
-    cmd = input('Enter command > ')
+    cmd = input('Enter magnet command (or: "q" to quit; "r" to repeat last cmd)> ')
 
     # Special control-loop commands:
     if cmd in QUIT:
@@ -51,27 +53,18 @@ while True:
         cmd = CMD_ALIASES[cmd]
 
     magnet.send_cmd(cmd)
-    print(magnet.read_buffer())
+    magnet.read_buffer()  # Prints (by default) and returns message
 
-# Lists to contain data:
-Vs = []
-Bs = []
+    if magnet.is_ramping():
+        magnet.run_ramp(dvm)  # Loops until ramp ends. Data is in magnet.Bs and magnet.Vs
+    # print('\n___________ END OF MAIN LOOP ____________\n')
+# END OF LOOP -----------------
 
-cmd = input('Enter ramp command > ')
-if cmd in CMD_ALIASES:
-    cmd = CMD_ALIASES[cmd]
-magnet.send_cmd(cmd)
-print(magnet.read_buffer())
+# Data storage:
+data_path = "G:/Shared drives/MSL - Electricity - Ongoing/QHR_CCC/Data_and_Analysis/Python_logging"
+filename = "test.csv"
+data_file = os.path.join(data_path, filename)
 
-print(f'DVM \tMagnet')
-
-while True:
-    V = dvm.read()
-    Vs.append(V)
-    B = dvm.read()
-    Bs.append(B)
-    print(f'{V} V\t{B} T')
-    if magnet.ramp_finished():
-        break
-
+# with open(data_file, w, newline=""):
+#     csv.
 print('FINISHED.')
